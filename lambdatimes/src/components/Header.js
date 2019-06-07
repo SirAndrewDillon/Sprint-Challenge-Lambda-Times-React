496bd2ceae4995d3916fbd590be570a9b0305291
Andrew Dillon <majik895@gmail.com>
2019-06-07 19:07:35 -0400

    images recoverd

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,48 +1,46 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 
-// Refactor this component to use styled components and not classNames. 
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
-
-const HeaderDiv = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
-flex-direction: row;
-background-color: #fff;
-border-bottom: 1px solid lightgrey;
-width: 100%;
-height: 120px;
-margin-top: 44px;
-padding-bottom: 15px;
-}
-@media (min-width: 1280px) {
-  width: 1280px;
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+  h1 {
+    font-size: 60px;
+    font-family: Didot, serif;
+    font-weight: bold;
+    flex: 8;
+    text-align: center;
+    color: #000;
+  }
 `;
 
-const HeaderHTag = styled.h1`
-font-size: 60px;
-  font-family: Didot, serif;
-  font-weight: bold;
-  flex: 8;
-  text-align: center;
-  color: #000;
-`;
-
-const HeaderSpan = styled.span`
-align-self: flex-end;
+const SpanDate = styled.span`
+  align-self: flex-end;
   font-size: 11px;
   font-weight: bold;
   letter-spacing: 1px;
-`;
-
-const HeaderSpanDate = styled(HeaderSpan)`
-margin-left: 25px;
+  margin-left: 25px;
   flex: 1;
 `;
 
-const HeaderSpanTemp = styled(HeaderSpan)`
-.header .temp {
+const Spantemp = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
   text-align: right;
   margin-right: 25px;
   flex: 1;
@@ -50,12 +48,12 @@ const HeaderSpanTemp = styled(HeaderSpan)`
 
 const Header = () => {
   return (
-    <HeaderDiv>
-      <HeaderSpanDate>SMARCH 32, 2018</HeaderSpanDate>
-      <HeaderHTag>Lambda Times</HeaderHTag>
-      <HeaderSpanTemp>98°</HeaderSpanTemp>
-    </HeaderDiv>
-  )
-}
+    <HeaderWrapper>
+      <SpanDate>SMARCH 32, 2018</SpanDate>
+      <h1>Lambda Times</h1>
+      <Spantemp>98°</Spantemp>
+    </HeaderWrapper>
+  );
+};
 
-export default Header
+export default Header;
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 
-// Refactor this component to use styled components and not classNames. 
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
-
-const Top = styled.div`
-width: 100%;
+const TopBarWrapper = styled.div`
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: none;
@@ -15,46 +14,48 @@ width: 100%;
   background-color: #333;
 `;
 
-const Container = styled.div `
-width: 100%;
-display: flex;
-justify-content: none;
-align-items: none;
-flex-direction: row;
-color: #fff;
-letter-spacing: 1px;
-padding: 0 10px;
-@media (min-width: 1280px) {
-    width: 1280px;
+const TopBarContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+  @media (min-width: 1280px) {
+     {
+      width: 1280px;
+    }
   }
 `;
 
-const ContainerLeft = styled.div`
-display: flex;
-justify-content: none;
-align-items: center;
-flex-direction: row;
-flex: 1;
-font-size: 11px;
+const TopBarContainerLeft = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
 `;
 
-const ContainerLeftSpan = styled.span`
-cursor: pointer;
-margin-right: 25%;
-font-weight: bold;
+const TopBarContainerLeftSpan = styled.span`
+  cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
 `;
 
-const ContainerCenter = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-flex-direction: row;
-flex: 3;
-font-size: 9px;
+const TopBarContainerCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
 `;
 
-const ContainerCenterSpan = styled.span`
-cursor: pointer;
+const TopBarContainerCenterSpan = styled.span`
+  cursor: pointer;
   margin-right: 5%;
   :last-child {
     margin-right: 0;
@@ -64,39 +65,42 @@ cursor: pointer;
   }
 `;
 
-const ContainerRight = styled.div`
-display: flex;
-justify-content: flex-end;
-align-items: center;
-flex-direction: row;
-flex: 1;
-font-size: 11px;
-font-weight: bold;
+const TopBarContainerRight = styled.div`
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+  margin-top: 15px;
 `;
 
-const ContainerRightSpan = styled.span`
-cursor: pointer;
+const TopBarContainerRightSpan = styled.span`
+  cursor: pointer;
+  
 `;
 
 const TopBar = () => {
   return (
-    <Top>
-      <Container>
-        <ContainerLeft>
-          <ContainerLeftSpan>TOPICS</ContainerLeftSpan><ContainerLeftSpan>SEARCH</ContainerLeftSpan>
-        </ContainerLeft>
-        <ContainerCenter>
-          <ContainerCenterSpan>GENERAL</ContainerCenterSpan>
-          <ContainerCenterSpan>BROWNBAG</ContainerCenterSpan>
-          <ContainerCenterSpan>RANDOM</ContainerCenterSpan>
-          <ContainerCenterSpan>MUSIC</ContainerCenterSpan><ContainerCenterSpan>ANNOUNCEMENTS</ContainerCenterSpan>
-        </ContainerCenter>
-        <ContainerRight>
-          <ContainerRightSpan>LOG IN</ContainerRightSpan>
-        </ContainerRight>
-      </Container>
-    </Top>
-  )
-}
+    <TopBarWrapper>
+      <TopBarContainer>
+        <TopBarContainerLeft>
+          <TopBarContainerLeftSpan>TOPICS</TopBarContainerLeftSpan>
+          <TopBarContainerLeftSpan>SEARCH</TopBarContainerLeftSpan>
+        </TopBarContainerLeft>
+        <TopBarContainerCenter>
+          <TopBarContainerCenterSpan>GENERAL</TopBarContainerCenterSpan>
+          <TopBarContainerCenterSpan>BROWNBAG</TopBarContainerCenterSpan>
+          <TopBarContainerCenterSpan>RANDOM</TopBarContainerCenterSpan>
+          <TopBarContainerCenterSpan>MUSIC</TopBarContainerCenterSpan>
+          <TopBarContainerCenterSpan>ANNOUNCEMENTS</TopBarContainerCenterSpan>
+        </TopBarContainerCenter>
+        <TopBarContainerRight>
+          <TopBarContainerRightSpan>LOG IN</TopBarContainerRightSpan>
+        </TopBarContainerRight>
+      </TopBarContainer>
+    </TopBarWrapper>
+  );
+};
 
 export default TopBar;

a6a21685c7c644f95337c714a4a517d7083cf427
Andrew Dillon <majik895@gmail.com>
2019-06-07 13:49:03 -0400

    tabs working failed to import prototypes

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
@@ -9,6 +10,7 @@ const Tab = props => {
     <div
       className={''}
       onClick={() => {
+        props.selectTabHandler(props.tab)
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
       }}
@@ -19,5 +21,11 @@ const Tab = props => {
 };
 
 // Make sure you include PropTypes on your props.
+Tab.propTypes={
+  tab: PropTypes.string,
+  selectedTab: PropTypes.string,
+  selectTabHandler: PropTypes.func.isRequired
+}
+
 
 export default Tab;

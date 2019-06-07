bd8e3951cd899325afa5cf72e1e210f6c98be1a1
Andrew Dillon <majik895@gmail.com>
2019-06-07 12:56:36 -0400

    fixed topBar imperfections

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,40 +1,35 @@
 import React from 'react';
-import styled, {css} from 'styled-components';
+import styled from "styled-components";
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
 const Top = styled.div`
 width: 100%;
-display: flex;
-justify-content: center;
-align-items: none;
-flex-direction: row;
-position: fixed;
-height: 44px;
-background-color: #333;
-`;
-
-
-
-const Container = styled.div`
-width: 100%;
   display: flex;
-  justify-content: none;
+  justify-content: center;
   align-items: none;
   flex-direction: row;
-  color: #fff;
-  letter-spacing: 1px;
-  padding: 0 10px;
-  @media (min-width: 1280px) {
-    .top-bar .container {
-      width: 1280px;
-    }
+  position: fixed;
+  height: 44px;
+  background-color: #333;
 `;
 
+const Container = styled.div `
+width: 100%;
+display: flex;
+justify-content: none;
+align-items: none;
+flex-direction: row;
+color: #fff;
+letter-spacing: 1px;
+padding: 0 10px;
+@media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
 
-
-const LeftContainer = styled.div`
+const ContainerLeft = styled.div`
 display: flex;
 justify-content: none;
 align-items: center;
@@ -43,34 +38,33 @@ flex: 1;
 font-size: 11px;
 `;
 
-const LeftSpan = styled(LeftContainer)`
+const ContainerLeftSpan = styled.span`
 cursor: pointer;
 margin-right: 25%;
 font-weight: bold;
 `;
 
-const CenterContainer = styled.div`
+const ContainerCenter = styled.div`
 display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: row;
-  flex: 3;
-  font-size: 9px;
+justify-content: center;
+align-items: center;
+flex-direction: row;
+flex: 3;
+font-size: 9px;
 `;
 
-const CenterSpan = styled(CenterContainer)`
+const ContainerCenterSpan = styled.span`
 cursor: pointer;
   margin-right: 5%;
-  
-  &:last-child {
+  :last-child {
     margin-right: 0;
   }
-  &:hover {
+  :hover {
     text-decoration: underline;
   }
 `;
 
-const RightContainer = styled.div`
+const ContainerRight = styled.div`
 display: flex;
 justify-content: flex-end;
 align-items: center;
@@ -78,26 +72,28 @@ flex-direction: row;
 flex: 1;
 font-size: 11px;
 font-weight: bold;
-${props => props.span && css`
-cursor: pointer;
-`}
 `;
 
-
+const ContainerRightSpan = styled.span`
+cursor: pointer;
+`;
 
 const TopBar = () => {
   return (
     <Top>
       <Container>
-        <LeftContainer>
-          <LeftSpan>TOPICS</LeftSpan><LeftSpan>SEARCH</LeftSpan>
-        </LeftContainer>
-        <CenterContainer>
-          <CenterSpan>GENERAL</CenterSpan><CenterSpan>BROWNBAG</CenterSpan><CenterSpan>RANDOM</CenterSpan><CenterSpan>MUSIC</CenterSpan><CenterSpan>ANNOUNCEMENTS</CenterSpan>
-        </CenterContainer>
-        <RightContainer>
-          <span>LOG IN</span>
-        </RightContainer>
+        <ContainerLeft>
+          <ContainerLeftSpan>TOPICS</ContainerLeftSpan><ContainerLeftSpan>SEARCH</ContainerLeftSpan>
+        </ContainerLeft>
+        <ContainerCenter>
+          <ContainerCenterSpan>GENERAL</ContainerCenterSpan>
+          <ContainerCenterSpan>BROWNBAG</ContainerCenterSpan>
+          <ContainerCenterSpan>RANDOM</ContainerCenterSpan>
+          <ContainerCenterSpan>MUSIC</ContainerCenterSpan><ContainerCenterSpan>ANNOUNCEMENTS</ContainerCenterSpan>
+        </ContainerCenter>
+        <ContainerRight>
+          <ContainerRightSpan>LOG IN</ContainerRightSpan>
+        </ContainerRight>
       </Container>
     </Top>
   )
